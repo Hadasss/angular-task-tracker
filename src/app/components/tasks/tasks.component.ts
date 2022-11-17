39decc7f2af8379ebbce9010f76207caba6556b9
Hadasss <hadas.gadish@gmail.com>
2022-11-16 17:00:18 -0700

    added mock db file + http client to angular, to enable http requests

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -16,4 +16,12 @@ export class TasksComponent implements OnInit {
     // void means that this function will not return anything.
     this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
   }
+
+  deleteTask(task: Task) {
+    this.taskService
+      .deleteTask(task)
+      .subscribe(
+        () => (this.tasks = this.tasks.filter((t) => t.id !== task.id))
+      );
+  }
 }
